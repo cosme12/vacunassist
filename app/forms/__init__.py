1f14587fdc2f640ecb72306c513f182e50d122da
Diego Lanciotti <aaadiego502@gmail.com>
2022-05-09 00:50:56 -0300

    Agrega campos de vacunas de forma dinamica

--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, EmailField, DateField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, EmailField, DateField, SelectField, FieldList
 from wtforms.validators import DataRequired, InputRequired, EqualTo
 
 
@@ -20,5 +20,9 @@ class RegistroForm(FlaskForm):
     fecha_de_nacimiento = DateField('Fecha de nacimiento', validators=[DataRequired('Este campo es requerido')])
     telefono = StringField('Teléfono', validators=[])    
     paciente_de_riesgo = BooleanField('¿Es paciente de riesgo?')
+    vacunas = FieldList(SelectField('¿Qué vacuna ya se aplicó?', choices=[("", "-"), ("covid1", "Covid 1era dosis"), ("covid2", "Covid 2da dosis"),
+                                                                ("fiebre_amarilla", "Fiebre amarilla"), ("gripe", "Gripe")]),
+                                                                min_entries=1, max_entries=5)
+    #vacunas = StringField('¿Qué vacuna ya se aplicó?', min_entries=1, max_entries=5)
     enviar = SubmitField('Registrarse')
 

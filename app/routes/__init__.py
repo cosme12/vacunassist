5ada433f4ba0420cb7ceb94ae7a73e9bb306c6dd
AyelenSarco <ayelensarco94@gmail.com>
2022-05-11 15:08:30 -0300

    correccion de identacion

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -67,10 +67,10 @@ def perfil():
 @app.route('/mis-turnos') # http://localhost:5000/mis-turnos
 @login_required
 def mis_turnos():
-        usuario = session['dni']
-        user_data = models.get_user_data(usuario)
-        mis_turnos = models.get_turnos_from_usuario(user_data['id']) 
-        return render_template ('mis_turnos.html', titulo='Mis turnos', usuario=usuario, mis_turnos=mis_turnos)
+    usuario = session['dni']
+    user_data = models.get_user_data(usuario)
+    mis_turnos = models.get_turnos_from_usuario(user_data['id']) 
+    return render_template ('mis_turnos.html', titulo='Mis turnos', usuario=usuario, mis_turnos=mis_turnos)
 
 @app.route('/mis-vacunas') # http://localhost:5000/mis-vacunas
 @login_required

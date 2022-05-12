8bab7eb0b82539895dc484b66256273103a25ba7
Diego Lanciotti <aaadiego502@gmail.com>
2022-05-12 16:24:21 -0300

    Agrega campo fecha a historial registro vacuna

--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, EmailField, DateField, SelectField, FieldList, IntegerField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, EmailField, DateField, SelectField, FieldList, FormField
 from wtforms.validators import DataRequired, InputRequired, EqualTo
 
 
@@ -8,6 +8,12 @@ class LoginForm(FlaskForm):
     password = PasswordField('Contraseña', validators=[DataRequired('Este campo es requerido')])
     token = StringField('Token')
     iniciar_sesion = SubmitField('Iniciar sesión')
+
+
+class HistorialVacunasForm(FlaskForm):
+    vacuna = SelectField('¿Qué vacuna ya se aplicó?', choices=[("", "-"), ("covid1", "Covid 1era dosis"), ("covid2", "Covid 2da dosis"),
+                                                                ("fiebre_amarilla", "Fiebre amarilla"), ("gripe", "Gripe")])
+    fecha_aplicacion = DateField('Fecha de aplicación')
 
 
 class RegistroForm(FlaskForm):
@@ -20,9 +26,7 @@ class RegistroForm(FlaskForm):
     fecha_de_nacimiento = DateField('Fecha de nacimiento', validators=[DataRequired('Este campo es requerido')])
     telefono = StringField('Teléfono', validators=[])    
     paciente_de_riesgo = BooleanField('¿Es paciente de riesgo?')
-    vacunas = FieldList(SelectField('¿Qué vacuna ya se aplicó?', choices=[("", "-"), ("covid1", "Covid 1era dosis"), ("covid2", "Covid 2da dosis"),
-                                                                ("fiebre_amarilla", "Fiebre amarilla"), ("gripe", "Gripe")]),
-                                                                min_entries=1, max_entries=5)
-    #vacunas = StringField('¿Qué vacuna ya se aplicó?', min_entries=1, max_entries=5)
+    vacunas = FieldList(FormField(HistorialVacunasForm), min_entries=1, max_entries=5)
     enviar = SubmitField('Registrarse')
+
 

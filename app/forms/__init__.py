698d15d0f4446abad29baa1bdfe41181ccfe2a14
Diego Lanciotti <aaadiego502@gmail.com>
2022-05-12 23:34:41 -0300

    Permite quitar vacunas y valida campos

--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm, Form
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, EmailField, DateField, SelectField, FieldList, FormField
-from wtforms.validators import DataRequired, InputRequired, EqualTo
+from wtforms.validators import DataRequired, InputRequired, EqualTo, Length, NumberRange, Regexp
 import datetime
 
 
@@ -18,16 +18,16 @@ class HistorialVacunasForm(Form):
 
 
 class RegistroForm(FlaskForm):
-    dni = StringField('DNI', validators=[DataRequired('Este campo es requerido')])
-    nombre = StringField('Nombre', validators=[DataRequired('Este campo es requerido')])
-    apellido = StringField('Apellido', validators=[DataRequired('Este campo es requerido')])
+    dni = StringField('DNI', validators=[DataRequired('Este campo es requerido'), Length(min=8, max=8, message='No es un dni válido')], render_kw={'onkeyup':'return validateNumber(event)'})
+    nombre = StringField('Nombre', validators=[DataRequired('Este campo es requerido')], render_kw={'onkeyup':'return validateChars(event)'})
+    apellido = StringField('Apellido', validators=[DataRequired('Este campo es requerido')], render_kw={'onkeyup':'return validateChars(event)'})
     email = EmailField('Email', validators=[DataRequired('Este campo es requerido')])
     password = PasswordField('Contraseña', [InputRequired(), EqualTo('confirmar', message='Las contraseñas deben coincidir')])
     confirmar  = PasswordField('Repetir contraseña')
     fecha_de_nacimiento = DateField('Fecha de nacimiento', validators=[DataRequired('Este campo es requerido')], render_kw={'min':'1900-01-01', 'max':datetime.datetime.now().strftime("%Y-%m-%d")})
     telefono = StringField('Teléfono', validators=[])    
     paciente_de_riesgo = BooleanField('¿Es paciente de riesgo?')
-    vacunas = FieldList(FormField(HistorialVacunasForm), min_entries=1, max_entries=5)
-    enviar = SubmitField('Registrarse')
+    vacunas = FieldList(FormField(HistorialVacunasForm), min_entries=0, max_entries=5)
+    enviar = SubmitField('Registrarse', render_kw={'onkeyup':'return validateChars(event)'})
 
 

f61210261ae72a7fe974d81cc3498aaef993ac76
Diego Lanciotti <aaadiego502@gmail.com>
2022-05-16 22:06:30 -0300

    Agrega test registro usuario con historial

--- tests/test_registro.py
+++ tests/test_registro.py
@@ -88,3 +88,67 @@ def test_registrar_paciente_sin_historial(client, create_db):
     assert paciente["paciente_de_riesgo"] == 1
     assert paciente["tipo"] == 1
 
+    # Verifica que no se hayan cargado ningun dato en vacunas aplicadas
+    conn = sqlite3.connect(flask_app.config['DATABASE'])
+    conn.row_factory = sqlite3.Row 
+    cursor = conn.cursor()
+    vacunas_aplicadas = cursor.execute("SELECT COUNT(*) FROM vacuna_aplicada").fetchone()
+    conn.close()
+    assert vacunas_aplicadas[0] == 0
+
+
+def test_registrar_paciente_con_historial(client, create_db):
+    response = client.post('/registro', data={
+        'dni': '00000001',
+        'nombre': 'Juan',
+        'apellido': 'Perez',
+        'email': 'juanperez@example.com',
+        'password': '12345',
+        'confirmar': '12345',
+        'fecha_de_nacimiento': '2000-01-01',
+        'telefono': '123456789',
+        'paciente_de_riesgo': True,
+        'vacunas-0-id_vacuna': '1',
+        'vacunas-0-fecha_aplicacion': '2020-01-01',
+        'vacunas-1-id_vacuna': '2',
+        'vacunas-1-fecha_aplicacion': '2020-01-02'
+    }, follow_redirects=True)
+    assert response.status_code == 200
+    #debuggear_respuesta(response.get_data(as_text=True))
+    assert response.request.path == '/login' # redirected to login
+    html = response.get_data(as_text=True)
+    assert f'El registro fue exitoso. Hemos enviado un mail a juanperez@example.com con un token que deberá usar junto con su contraseña para iniciar sesión.' in html
+
+    # Verifica que los datos del usuario se hayan guardado en la base de datos
+    conn = sqlite3.connect(flask_app.config['DATABASE'])
+    conn.row_factory = sqlite3.Row 
+    cursor = conn.cursor()
+    paciente = cursor.execute("SELECT * FROM usuario WHERE dni = '00000001'").fetchone()
+    conn.close()
+    assert paciente is not None
+    assert paciente["nombre"] == 'Juan'
+    assert paciente["apellido"] == 'Perez'
+    assert paciente["password"] == '5994471abb01112afcc18159f6cc74b4f511b99806da59b3caf5a9c173cacfc5'
+    assert paciente["email"] == 'juanperez@example.com'
+    assert paciente["fecha_de_nacimiento"] == '2000-01-01'
+    assert paciente["telefono"] == '123456789'
+    assert paciente["paciente_de_riesgo"] == 1
+    assert paciente["tipo"] == 1
+
+    # Verifica que se hayan cargado las vacunas aplicadas
+    conn = sqlite3.connect(flask_app.config['DATABASE'])
+    conn.row_factory = sqlite3.Row 
+    cursor = conn.cursor()
+    vacunas_aplicadas = cursor.execute("SELECT * FROM vacuna_aplicada WHERE id_usuario = 1").fetchall()
+    conn.close()
+    assert len(vacunas_aplicadas) == 2
+    assert vacunas_aplicadas[0]["id_usuario"] == 1
+    assert vacunas_aplicadas[0]["id_vacuna"] == 1
+    assert vacunas_aplicadas[0]["fecha"] == '2020-01-01'
+    assert vacunas_aplicadas[1]["id_usuario"] == 1
+    assert vacunas_aplicadas[1]["id_vacuna"] == 2
+    assert vacunas_aplicadas[1]["fecha"] == '2020-01-02'
+    
+
+
+

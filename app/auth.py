a62ad1e0cc5e6e65f563423dac7712880b9b7ab2
Diego Lanciotti <aaadiego502@gmail.com>
2022-05-21 19:47:08 -0300

    Redirige a reservar turno covid

--- app/auth.py
+++ app/auth.py
@@ -12,14 +12,14 @@ def login_required(func):
         if 'dni' not in session:
             return redirect(url_for('login'))
         else:
-            if request.path != url_for('perfil') and request.path != url_for('logout') and 'eliminar_cuenta' not in request.path:
+            if request.path != url_for('perfil') and request.path != url_for('logout') and 'eliminar_cuenta' not in request.path and 'sacar-turno' not in request.path:
                 usuario = get_user_data(session['dni'])
                 edad = edad_de_usuario(usuario['id'])[0]
                 tiene_turno_covid = tiene_appointment_covid1_from_user(usuario['id'])
                 vucuna_covid_1 = tiene_vacuna_aplicada(usuario['id'], 4)
                 if not vucuna_covid_1 and not tiene_turno_covid and (usuario['paciente_de_riesgo'] or edad > 60):
-                    flash("Para poder acceder al resto del sitio debe reservar un turno para aplicarse la primera dosis de la vacuna de COVID-19.", "danger")
-                    return redirect(url_for('perfil'))
+                    flash("Antes de poder acceder al resto del sitio debe reservar un turno para aplicarse la primera dosis de la vacuna de COVID-19.", "danger")
+                    return redirect(url_for('sacar_turno', id_vacuna=4))
         return func(*args, **kwargs)
     return decorated_function
 

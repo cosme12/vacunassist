a4dece5712d408bd9771a7ef8869dfe49f9f0d30
Diego Lanciotti <aaadiego502@gmail.com>
2022-05-21 18:29:05 -0300

    Obliga al usuario a sacar vacuna covid1

--- app/auth.py
+++ app/auth.py
@@ -1,5 +1,9 @@
-from flask import redirect, url_for, session
+from flask import redirect, url_for, session, request, flash
 from functools import wraps
+from app.models.turnos import get_appointment_from_user
+from app.models.usuarios import edad_de_usuario, get_user_data
+from app.models.turnos import tiene_appointment_covid1_from_user
+from app.models. vacunas_aplicadas import tiene_vacuna_aplicada
 
 
 def login_required(func):
@@ -7,6 +11,15 @@ def login_required(func):
     def decorated_function(*args, **kwargs):
         if 'dni' not in session:
             return redirect(url_for('login'))
+        else:
+            if request.path != url_for('perfil') and request.path != url_for('logout') and 'eliminar_cuenta' not in request.path:
+                usuario = get_user_data(session['dni'])
+                edad = edad_de_usuario(usuario['id'])[0]
+                tiene_turno_covid = tiene_appointment_covid1_from_user(usuario['id'])
+                vucuna_covid_1 = tiene_vacuna_aplicada(usuario['id'], 4)
+                if not vucuna_covid_1 and not tiene_turno_covid and (usuario['paciente_de_riesgo'] or edad > 60):
+                    flash("Para poder acceder al resto del sitio debe reservar un turno para aplicarse la primera dosis de la vacuna de COVID-19.", "danger")
+                    return redirect(url_for('perfil'))
         return func(*args, **kwargs)
     return decorated_function
 
--- app/models/turnos.py
+++ app/models/turnos.py
@@ -26,6 +26,25 @@ def get_appointment_from_user(id):
     return turnos
 
 
+def tiene_appointment_covid1_from_user(id):
+    """
+    Indica si tiene turno de covid1 reservado para el usuario con id=id
+    """
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    turnos = cursor.execute("""SELECT COUNT(*) FROM turno 
+                                INNER JOIN vacuna as e ON e.id = turno.id_vacuna  
+                                INNER JOIN zona as z ON z.id=turno.id_zona 
+                                WHERE id_usuario =? AND estado=? AND id_vacuna=?;
+                                """, (id, 1, 4)).fetchone()
+    if turnos[0] > 0:
+        turnos = True
+    else:
+        turnos = False
+    conn.close()
+    return turnos
+
+
 def cancel_appointment(id):
     conn = get_db_connection()
     cursor = conn.cursor()

968d5f1e4e5e39487edd1309c0a1db4d67eaac50
Diego Lanciotti <aaadiego502@gmail.com>
2022-05-22 20:12:34 -0300

    Corrige typo en mensaje vacuna repetida

--- tests/test_registro.py
+++ tests/test_registro.py
@@ -97,6 +97,41 @@ def test_registrar_paciente_sin_historial(client, create_db):
     assert vacunas_aplicadas[0] == 0
 
 
+def test_registrar_paciente_sin_historial_identidad_incorrecta(client, create_db):
+    response = client.post('/registro', data={
+        'dni': '00000000',
+        'nombre': 'Juan',
+        'apellido': 'Perez',
+        'email': 'juanperez@example.com',
+        'password': '12345',
+        'confirmar': '12345',
+        'fecha_de_nacimiento': '2000-01-01',
+        'telefono': '123456789',
+        'paciente_de_riesgo': True
+    }, follow_redirects=True)
+    assert response.status_code == 200
+    #debuggear_respuesta(response.get_data(as_text=True))
+    assert response.request.path == '/registro' # redirected to login
+    html = response.get_data(as_text=True)
+    assert f'No pudo validarse su identidad' in html
+
+    # Verifica que los datos del usuario se hayan guardado en la base de datos
+    conn = sqlite3.connect(flask_app.config['DATABASE'])
+    conn.row_factory = sqlite3.Row 
+    cursor = conn.cursor()
+    paciente = cursor.execute("SELECT * FROM usuario WHERE dni = '00000001'").fetchone()
+    conn.close()
+    assert paciente is None
+
+    # Verifica que no se hayan cargado ningun dato en vacunas aplicadas
+    conn = sqlite3.connect(flask_app.config['DATABASE'])
+    conn.row_factory = sqlite3.Row 
+    cursor = conn.cursor()
+    vacunas_aplicadas = cursor.execute("SELECT COUNT(*) FROM vacuna_aplicada").fetchone()
+    conn.close()
+    assert vacunas_aplicadas[0] == 0
+
+
 def test_registrar_paciente_con_historial(client, create_db):
     response = client.post('/registro', data={
         'dni': '00000001',
@@ -168,7 +203,7 @@ def test_registrar_paciente_con_historial_vacunas_repetidas(client, create_db):
     }, follow_redirects=True)
     assert response.status_code == 200
     html = response.get_data(as_text=True)
-    assert f'No pueden seleccionar más de una vez la misma vacuna.' in html
+    assert f'No puede seleccionar más de una vez la misma vacuna.' in html
 
     # Verifica que los datos del usuario NO se hayan guardado en la base de datos
     conn = sqlite3.connect(flask_app.config['DATABASE'])

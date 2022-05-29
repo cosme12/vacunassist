9bff998487abad7da88e2f9745ee9f012270a37b
AyelenSarco <ayelensarco94@gmail.com>
2022-05-29 13:49:15 -0300

    saco restricciones para empleados en el loguin

--- app/auth.py
+++ app/auth.py
@@ -12,19 +12,20 @@ def login_required(func):
         if 'dni' not in session:
             return redirect(url_for('login'))
         else:
-            if request.path != url_for('perfil') and request.path != url_for('logout') and 'eliminar_cuenta' not in request.path and 'sacar-turno' not in request.path:
-                usuario = get_user_data(session['dni'])
-                edad = edad_de_usuario(usuario['id'])
-                tiene_turno_covid = tiene_appointment_covid1_from_user(usuario['id'])
-                vucuna_covid_1 = tiene_vacuna_aplicada(usuario['id'], 4)
-                vacuna_gripe = tiene_vacuna_gripe(usuario['id'])
-                tiene_turno_gripe = tiene_turno_pendiente(usuario['id'], 1)
-                if not vucuna_covid_1 and not tiene_turno_covid and (usuario['paciente_de_riesgo'] or edad > 60):
-                    flash("Antes de poder acceder al resto del sitio debe reservar un turno para aplicarse la primera dosis de la vacuna de COVID-19.", "danger")
-                    return redirect(url_for('sacar_turno', id_vacuna=4))
-                elif not vacuna_gripe and not tiene_turno_gripe and edad > 60:
-                    flash("Antes de poder acceder al resto del sitio debe reservar un turno para aplicarse la vacuna de la GRIPE.", "danger")
-                    return redirect(url_for('sacar_turno', id_vacuna=1))
+            if session["tipo"] == 1:
+                if request.path != url_for('perfil') and request.path != url_for('logout') and 'eliminar_cuenta' not in request.path and 'sacar-turno' not in request.path:
+                    usuario = get_user_data(session['dni'])
+                    edad = edad_de_usuario(usuario['id'])
+                    tiene_turno_covid = tiene_appointment_covid1_from_user(usuario['id'])
+                    vucuna_covid_1 = tiene_vacuna_aplicada(usuario['id'], 4)
+                    vacuna_gripe = tiene_vacuna_gripe(usuario['id'])
+                    tiene_turno_gripe = tiene_turno_pendiente(usuario['id'], 1)
+                    if not vucuna_covid_1 and not tiene_turno_covid and (usuario['paciente_de_riesgo'] or edad > 60):
+                        flash("Antes de poder acceder al resto del sitio debe reservar un turno para aplicarse la primera dosis de la vacuna de COVID-19.", "danger")
+                        return redirect(url_for('sacar_turno', id_vacuna=4))
+                    elif not vacuna_gripe and not tiene_turno_gripe and edad > 60:
+                        flash("Antes de poder acceder al resto del sitio debe reservar un turno para aplicarse la vacuna de la GRIPE.", "danger")
+                        return redirect(url_for('sacar_turno', id_vacuna=1))
         return func(*args, **kwargs)
     return decorated_function
 

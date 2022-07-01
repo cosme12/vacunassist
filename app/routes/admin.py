73f389744a035fe244cc24c3907e8d50175da402
AyelenSarco <ayelensarco94@gmail.com>
2022-06-30 21:43:06 -0300

    mensaje de succes confirmar/rechazar turno

--- app/routes/admin.py
+++ app/routes/admin.py
@@ -187,6 +187,7 @@ def aprobar_turno(id):
     models.aprobar_turno(id)
     if app.config['EMAIL_ENABLED']:
                 enviar_email(usuario['email'], "Vacunassist - Aprobacion de turno Fiebre Amarilla", f"Hola {usuario['nombre']}\n\nSu turno para la vacuna la Fiebre amarilla el dia {turno['fecha']}, a las {turno['hora']} horas, en zona{turno['nombre']} {turno['direccion']} a sido aprobado.\n")
+    flash("Se aprobó el turno correctamente. Se envió un email informando la actualización de estado del turno al paciente correspondiente.", "success")
     return redirect(url_for('listado_pendientes_fiebre_amarilla'))
 
 
@@ -197,6 +198,7 @@ def rechazar_turno(id):
     usuario = models.get_user_data_por_id(turno['id_usuario'])
     models.cancel_appointment(id,5)
     if app.config['EMAIL_ENABLED']:
-                enviar_email(usuario['email'], "Vacunassist - Aprobacion de turno Fiebre Amarilla", f"Hola {usuario['nombre']}\n\nSu turno para la vacuna la Fiebre amarilla el dia {turno['fecha']}, a las {turno['hora']} horas, en zona{turno['nombre']} {turno['direccion']} a sido rechazado.\n Ante cualquier consulta comuniquese al 221- 5412345")
+                enviar_email(usuario['email'], "Vacunassist - Rechazo de turno Fiebre Amarilla", f"Hola {usuario['nombre']}\n\nSu turno para la vacuna la Fiebre amarilla el dia {turno['fecha']}, a las {turno['hora']} horas, en zona{turno['nombre']} {turno['direccion']} a sido rechazado.\n Ante cualquier consulta comuniquese al 221- 5412345")
+    flash("Se rechazó el turno correctamente. Se envió un email informando la actualización de estado del turno al paciente correspondiente.", "success")
     return redirect(url_for('listado_pendientes_fiebre_amarilla'))
 

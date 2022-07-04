555cc8e15206505701f16ab4ab4d5bd741d621fc
Diego Lanciotti <aaadiego502@gmail.com>
2022-07-04 19:29:00 -0300

    Agrega  un try al envio de emails

--- app/handlers/email_enviar.py
+++ app/handlers/email_enviar.py
@@ -10,38 +10,40 @@ import base64
 
 
 def enviar_email(receiver,subject, msg):
-
-    #subject = "Asunto del mensaje prueba"
-    #msg = "Un hermoso mensaje de prueba :D"
-    sender = "aaacosme502@gmail.com"
-    #receiver = "aaadiego502@gmail.com"
-
-    SCOPES = ['https://www.googleapis.com/auth/gmail.modify']
-    creds = None
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
-            creds = pickle.load(token)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
-    service = build('gmail', 'v1', credentials=creds)
-    message = MIMEText(msg)
-    message['to'] = receiver
-    message['from'] = sender
-    message['subject'] = subject
-    raw = base64.urlsafe_b64encode(message.as_bytes())
-    raw = raw.decode()
-    body = {'raw' : raw}
     try:
-        message = (service.users().messages().send(userId='me', body=body).execute())
-    except Exception as e:
-        print(e)
+        #subject = "Asunto del mensaje prueba"
+        #msg = "Un hermoso mensaje de prueba :D"
+        sender = "aaacosme502@gmail.com"
+        #receiver = "aaadiego502@gmail.com"
+
+        SCOPES = ['https://www.googleapis.com/auth/gmail.modify']
+        creds = None
+        if os.path.exists('token.pickle'):
+            with open('token.pickle', 'rb') as token:
+                creds = pickle.load(token)
+        # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'credentials.json', SCOPES)
+                creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open('token.pickle', 'wb') as token:
+                pickle.dump(creds, token)
+        service = build('gmail', 'v1', credentials=creds)
+        message = MIMEText(msg)
+        message['to'] = receiver
+        message['from'] = sender
+        message['subject'] = subject
+        raw = base64.urlsafe_b64encode(message.as_bytes())
+        raw = raw.decode()
+        body = {'raw' : raw}
+        try:
+            message = (service.users().messages().send(userId='me', body=body).execute())
+        except Exception as e:
+            print(e)
+    except:
+        pass
 
